f56444ae0b39453a2e66e23aed1d6ebbc3fcb673
BIT-MJY <mjy980625@1663.com>
2022-03-11 09:32:47 +0800

    C++ implemention

--- OT_libtorch/gen_libtorch_model.py
+++ OT_libtorch/gen_libtorch_model.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# Developed by Junyi Ma, Xieyuanli Chen, and Jun Zhang
+# This file is covered by the LICENSE file in the root of the project OverlapTransformer:
+# https://github.com/haomo-ai/OverlapTransformer/
+# Brief: Generate OT model for Libtorch
+
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,7 +12,7 @@ import numpy as np
 from modules.feature_extracter_without_delta_layer import featureExtracter
 
 
-
+# change the path
 checkpoint = torch.load("/home/mjy/dev/OverlapNet++/tools/amodel_transformer_depth_only19.pth.tar")
 amodel = featureExtracter(channels=1, use_transformer=True)
 amodel.load_state_dict(checkpoint['state_dict']) 
@@ -18,5 +25,3 @@ example = example.cuda()
 # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
 traced_script_module = torch.jit.trace(amodel, example)
 traced_script_module.save("./overlapTransformer.pt")
-
-
