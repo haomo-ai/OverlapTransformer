c30e1f7ae9ca81dd13d77d20a468562687215432
BIT-MJY <mjy980625@1663.com>
2022-07-06 20:02:54 +0800

    revise gen_libtorch_model.py

--- OT_libtorch/gen_libtorch_model.py
+++ OT_libtorch/gen_libtorch_model.py
@@ -4,23 +4,41 @@
 # https://github.com/haomo-ai/OverlapTransformer/
 # Brief: Generate OT model for Libtorch
 
+import os
+import sys
+p = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
+if p not in sys.path:
+    sys.path.append(p)
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from modules.feature_extracter_without_delta_layer import featureExtracter
+from modules.overlap_transformer import featureExtracter
+import yaml
+
+# load config ================================================================
+config_filename = '../config/config.yml'
+config = yaml.safe_load(open(config_filename))
+test_weights = config["test_config"]["test_weights"]
+# ============================================================================
 
 
-# change the path
-checkpoint = torch.load("/home/mjy/dev/OverlapNet++/tools/amodel_transformer_depth_only19.pth.tar")
-amodel = featureExtracter(channels=1, use_transformer=True)
+print(test_weights)
+checkpoint = torch.load(test_weights)
+# for KITTI
+amodel = featureExtracter(height=64, width=900, channels=1, use_transformer=True)
+# for Haomo
+# amodel = featureExtracter(height=32, width=900, channels=1, use_transformer=True)
 amodel.load_state_dict(checkpoint['state_dict']) 
 
 amodel.cuda()
 amodel.eval()
 
-example = torch.rand(1, 1, 32, 900)
+# 64 for KITTI
+example = torch.rand(1, 1, 64, 900)   
+# 32 for Haomo
+# example = torch.rand(1, 1, 32, 900)   
 example = example.cuda()
 # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
 traced_script_module = torch.jit.trace(amodel, example)
